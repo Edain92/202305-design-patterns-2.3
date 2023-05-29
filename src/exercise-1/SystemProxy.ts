595b4c8951c54694bf09b011ffd52cef3ca9ae27
Edäin <mpizarrocuevas@gmail.com>
2023-05-29 18:23:39 +0200

    fix: todos after correction

--- src/exercise-1/SystemProxy.ts
+++ src/exercise-1/SystemProxy.ts
@@ -1,11 +1,16 @@
 export class SystemProxy implements SystemFunction {
-  #function : SystemFunction;
+  #function: SystemFunction;
 
   constructor(systemFunction: SystemFunction) {
     this.#function = systemFunction;
   }
 
+  /* TODO: guardar resultado en variable, el doSomething del proxy solo llama a this.#function.doSomething()
+  si su variable es undefined/null */
+
   doSomething(): string {
     return this.#function.doSomething()
   }
 }
+
+/* TODO: si la función tuviese parametros a recibir se podría crear un diccionario que asocie los resultados */
